33ab1cf7d11880cd0dfc73d07a1338d3cf3cfa77
apla77 <verocidade77@gmail.com>
2019-05-30 20:29:55 -0300

    Update WebSecurityConfig

--- src/main/java/br/edu/ifrn/livraria/config/WebSecurityConfig.java
+++ src/main/java/br/edu/ifrn/livraria/config/WebSecurityConfig.java
@@ -43,6 +43,7 @@ public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
 				.antMatchers("/css/**").permitAll()
 				.antMatchers("/distribution/**").permitAll()
 				.antMatchers("/js/**").permitAll()
+				.antMatchers(HttpMethod.GET, "home").permitAll()
 				.antMatchers(HttpMethod.GET, "/").permitAll()
 				.antMatchers(HttpMethod.GET, "/cadastrar").permitAll()
 				.anyRequest().authenticated()
